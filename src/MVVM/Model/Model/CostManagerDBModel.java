de82a0e168938671680e5c3a7140dd196208f95f
Daniel-Gelfand <76646067+Daniel-Gelfand@users.noreply.github.com>
2022-01-11 19:44:12 +0200

    DB model comments!

--- src/MVVM/Model/Model/CostManagerDBModel.java
+++ src/MVVM/Model/Model/CostManagerDBModel.java
@@ -10,22 +10,35 @@ import java.util.List;
 
 /**
  *
- * This Class implements IModel, responsible for transitions between database to CostManager System
- *
+ * This Class implements IModel, responsible for transitions between {MyPhpAdmin} database to CostManager System
+ * by using SQL queries and Jar Driver files.
+ * Methods:
+ *  (1) CostManagerDBModel(),
+ *  (2) setupNewAccount(Account account),
+ *  (3) loginToAccount(Account account),
+ *  (4) addCategory(Category category, Account account),
+ *  (5) getCategories(Account account),
+ *  (6) addNewCost(Cost cost),
+ *  (7) getReport(Account account, Date start, Date end).
  */
 
 
 public class CostManagerDBModel implements IModel {
 
-    // This List not in use yet.
+    // This member holds list of categories.
     private List<Category> categories = new LinkedList<>();
 
-    // login details to sql server
+    // These members are account details to log in into MyPhpAdmin account.
     private String url = "jdbc:mysql://localhost:3306/costmanager";
     private String username = "CostManager";
     private String password = "CostManager";
 
-    // This Ctor connect to database Sql server.
+
+    /**
+     * 'CostManagerDBModel' is a constructor.
+     * In this constructor we log in to MyPhpAdmin database
+     * @throws CostManagerExceptions
+     */
     public CostManagerDBModel() throws CostManagerExceptions {
 
 
@@ -39,7 +52,13 @@ public class CostManagerDBModel implements IModel {
         }
     }
 
-    // This method set new account in database.
+
+    /**
+     * 'setupNewAccount' is a methoad that insert into 'accounts_db' database new account details by using
+     * sql queries.
+     * @param account username
+     * @throws CostManagerExceptions
+     */
     @Override
     public void setupNewAccount(Account account) throws CostManagerExceptions {
 
@@ -48,11 +67,12 @@ public class CostManagerDBModel implements IModel {
                 Connection costManagerConnection = DriverManager.getConnection(url, username, password);
                 Statement statement = costManagerConnection.createStatement();
                 ) {
-
+            //SQL queries that insert into 'accounts_db' database new username and password.
             String query = "insert into accounts_db" + "(usernames,passwords)"
                     + "values (?,?)";
 
             PreparedStatement preparedStmt = costManagerConnection.prepareStatement(query);
+            // Sets to column 1 username and to column 2 password.
             preparedStmt.setString(1, account.getUsername());
             preparedStmt.setString(2, account.getPassword());
             preparedStmt.execute();
@@ -62,7 +82,14 @@ public class CostManagerDBModel implements IModel {
         }
     }
 
-    // This method search account in database and returns true or false in accordance.
+
+    /**
+     * 'loginToAccount' is a methoad that log in a specific account and check if the username and password is valid on database
+     * by sql queries.
+     * @param account username
+     * @return true / false
+     * @throws CostManagerExceptions
+     */
     @Override
     public boolean loginToAccount(Account account) throws CostManagerExceptions {
         boolean isRegistered = false;
@@ -73,7 +100,7 @@ public class CostManagerDBModel implements IModel {
                 ){
             ResultSet resultSet;
             if (account.getUsername() != null && account.getPassword() != null) {
-
+                //SQL queries that check if username and password are exists in database.
                 String sql = "Select * from accounts_db Where usernames='" + account.getUsername() + "' and passwords='" + account.getPassword() + "'";
                 resultSet = statement.executeQuery(sql);
 
@@ -97,7 +124,13 @@ public class CostManagerDBModel implements IModel {
         return isRegistered;
     }
 
-    // This method not in use yet.
+    /**
+     * 'addCategory' is a methoad that insert to 'categories_dbtest' database new specific category
+     * by using sql queries.
+     * @param category category
+     * @param account username
+     * @throws CostManagerExceptions
+     */
     @Override
     public void addCategory(Category category, Account account) throws CostManagerExceptions {
 
@@ -105,10 +138,11 @@ public class CostManagerDBModel implements IModel {
                 Connection costManagerConnection = DriverManager.getConnection(url, username, password);
                 Statement statement = costManagerConnection.createStatement();
                 ) {
-
+            //SQL queries that insert to 'categories_dbtest' category to specific username.
             String query = "insert into categories_dbtest" + "(usernames, categories)" + "values (?,?)";
             PreparedStatement preparedStmt = costManagerConnection.prepareStatement(query);
             System.out.println("Inserted To DB!");
+            // Sets to column 1 username and to column 2 category.
             preparedStmt.setString(1, account.getUsername());
             preparedStmt.setString(2, category.getCategory());
             preparedStmt.execute();
@@ -117,6 +151,13 @@ public class CostManagerDBModel implements IModel {
         }
     }
 
+    /**
+     * 'getCategories' is a methoad that get all categories to specific username
+     * by using sql queries.
+     * @param account username
+     * @return categories list
+     * @throws CostManagerExceptions
+     */
     @Override
     public LinkedList<Category> getCategories(Account account) throws CostManagerExceptions {
 
@@ -129,6 +170,7 @@ public class CostManagerDBModel implements IModel {
                     ResultSet resultSet;
                     LinkedList<Category> categories = new LinkedList<>();
                     System.out.println("Connected To DB!");
+                    //SQL queries that give to specific username his all categories.
                     String sql = "SELECT categories FROM categories_dbtest WHERE usernames='" + account.getUsername() + "'";
                     resultSet = statement.executeQuery(sql);
                     while (resultSet.next()) {
@@ -145,7 +187,13 @@ public class CostManagerDBModel implements IModel {
         }
     }
 
-    // This method add new cost to the database.
+
+    /**
+     * 'addNewCost' is a method that adding new cost to specific username
+     * by using sql queries.
+     * @param cost cost details
+     * @throws CostManagerExceptions
+     */
     @Override
     public void addNewCost(Cost cost) throws CostManagerExceptions{
 
@@ -153,10 +201,11 @@ public class CostManagerDBModel implements IModel {
                 Connection costManagerConnection = DriverManager.getConnection(url, username, password);
                 Statement statement = costManagerConnection.createStatement();
                 ){
-
+            //SQL queries that insert into 'main_db' who is the username,categories,descripiton,cost amount,currency and date.
             String query = "insert into main_db" + "(usernames,categories,description,cost,currency,date)"
                     + "values (?,?,?,?,?,?)";
             PreparedStatement preparedStmt = costManagerConnection.prepareStatement(query);
+            // Sets to column 1 username , column 2 Categories, column 3 Description, column 4 cost amount, column 5 currency,column 6 date.
             preparedStmt.setString(1, cost.getUsernames());
             preparedStmt.setString(2, cost.getCategories());
             preparedStmt.setString(3, cost.getDescription());
@@ -169,7 +218,15 @@ public class CostManagerDBModel implements IModel {
         }
     }
 
-    // This method returns reports about costs by selected dates.
+
+    /**
+     * 'getReport' is a method that get to specific account report about all his costs filtering by date.
+     * @param account username
+     * @param start from date
+     * @param end to date
+     * @return
+     * @throws CostManagerExceptions
+     */
     @Override
     public LinkedList<Cost> getReport(Account account, Date start, Date end) throws CostManagerExceptions {
         try (
